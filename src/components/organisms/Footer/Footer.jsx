185421625af7009119070769cebe215723b2020a
Rayhan <rayhanprima99@gmail.com>
2022-09-28 17:53:39 +0700

    chore (Footer): handle responsive

--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -15,7 +15,7 @@ export const Footer = () => {
 			<div className="relative bg-black bg-opacity-70">
 				<Container className="py-32 text-white text-center space-y-8">
 					<div className="space-y-4">
-						<h1 className="text-5xl font-bold leading-tight">Droidjam Indonesia 15th October 2022</h1>
+						<h1 className="text-4xl lg:text-5xl font-bold leading-tight">Droidjam Indonesia 15th October 2022</h1>
 						<div>
 							Call for sponsorship opportunities, contact{' '}
 							<a className="text-blue-400 hover:text-blue-500" href="mailto:info@droidjam.id">
