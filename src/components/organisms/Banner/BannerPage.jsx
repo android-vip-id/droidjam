63297cb02e1309a28c5b68ef51240524adeabd5e
Rayhan <rayhanprima99@gmail.com>
2022-09-28 19:25:13 +0700

    chore: add button props to BannerPage & implement to Schedule page

--- src/components/organisms/Banner/BannerPage.jsx
+++ src/components/organisms/Banner/BannerPage.jsx
@@ -1,6 +1,7 @@
-import { Container } from '@/components/atoms';
+import { Button, Container } from '@/components/atoms';
+import { FiCalendar } from 'react-icons/fi';
 
-export const BannerPage = ({ title, description }) => {
+export const BannerPage = ({ title, description, buttonText, onClickButton }) => {
 	return (
 		<section className="relative bg-black pt-40 pb-32">
 			<div className="absolute left-0 top-0 w-full h-full flex items-end justify-center">
@@ -12,14 +13,21 @@ export const BannerPage = ({ title, description }) => {
 			</div>
 
 			<Container className="relative">
-				<div className="text-white space-y-3">
-					<h1
-						className="text-5xl font-bold bg-gradient-primary bg-clip-text"
-						style={{ WebkitTextFillColor: 'transparent' }}
-					>
-						{title}
-					</h1>
-					<p className="text-lg opacity-50 lg:w-3/4">{description}</p>
+				<div className="space-y-8">
+					<div className="text-white space-y-3">
+						<h1
+							className="text-5xl font-bold bg-gradient-primary bg-clip-text"
+							style={{ WebkitTextFillColor: 'transparent' }}
+						>
+							{title}
+						</h1>
+						<p className="text-lg opacity-50 lg:w-3/4">{description}</p>
+					</div>
+					{buttonText && (
+						<Button className="px-6 py-3 space-x-3" leftIcon={<FiCalendar size={18} />} onClick={onClickButton}>
+							{buttonText}
+						</Button>
+					)}
 				</div>
 			</Container>
 		</section>
--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,13 +1,16 @@
 import { Fade } from 'react-reveal';
 import { Container } from '@/components/atoms';
 import { BannerPage, ScheduleList } from '@/components/organisms';
+import { DATA_EVENT } from '@/datas';
 
 export const Schedule = () => {
 	return (
 		<main>
 			<BannerPage
 				title="Schedule"
-				description="Follow code demonstrations by our expert speakers on 4 different tracks. Check out the schedule below and don't forget to mark your calendar so that you don't miss out on any sessions."
+				description="Follow code demonstrations by our expert speakers. Check out the schedule below and don't forget to mark your calendar so that you don't miss out on any sessions."
+				buttonText="Remind me"
+				onClickButton={() => window.open(DATA_EVENT.CALENDAR.REMINDER_URL, '_blank')}
 			/>
 			<Fade>
 				<Container className="py-20">
