46c5bbf29161b5df0f526248d5e2da62e0c38bee
Rayhan <rayhanprima99@gmail.com>
2022-09-29 00:32:55 +0700

    chore (Footer): update spacing & responsive

--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -13,9 +13,9 @@ export const Footer = () => {
 				/>
 			</div>
 			<div className="relative bg-black bg-opacity-70">
-				<Container className="py-32 text-white text-center space-y-8">
+				<Container className="py-20 text-white text-center space-y-8">
 					<div className="space-y-4">
-						<h1 className="text-4xl lg:text-5xl font-bold leading-tight">Droidjam Indonesia 15th October 2022</h1>
+						<h1 className="text-3xl lg:text-4xl font-bold leading-tight">Droidjam Indonesia 15th October 2022</h1>
 						<div>
 							Call for sponsorship opportunities, contact{' '}
 							<a className="text-blue-400 hover:text-blue-500" href="mailto:info@droidjam.id">
