4a5b33dbc761eed54f788ba8dc35d18b4c277b5e
Rayhan <rayhanprima99@gmail.com>
2022-10-02 17:17:38 +0700

    chore: add scroll to top when route's changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Home, Schedule, Speaker } from '@/pages';
 import { Header, Footer } from './components/organisms';
 import { SpeakerDetail } from './pages/Speaker/SpeakerDetail';
 import { ROUTES } from './routes';
+import { useEffect } from 'react';
 
 function App() {
+	const { pathname } = useLocation();
+	useEffect(() => {
+		document.documentElement.scrollTo({ top: 0, left: 0 });
+	}, [pathname]);
+
 	return (
 		<div>
 			<Header />
