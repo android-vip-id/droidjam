b6ea8f7228d96541be821661a905ca89cb977441
Rayhan <rayhanprima99@gmail.com>
2022-10-05 12:03:07 +0700

    chore (Sponsor): enlarge platinum & gold sponsors logo

--- src/pages/Home/components/HomeSponsors.jsx
+++ src/pages/Home/components/HomeSponsors.jsx
@@ -35,7 +35,7 @@ export const HomeSponsors = (type) => {
 						<div className="flex flex-wrap gap-4 md:gap-12 items-center justify-center">
 							{PLATINUM_SPONSORS.map((sponsor) => (
 								<div onClick={() => handleClick(sponsor)} className={`${sponsor.url ? 'cursor-pointer' : ''}`}>
-									<img className="w-36 lg:w-40" src={sponsor.image} alt={sponsor.name} />
+									<img className="w-48" src={sponsor.image} alt={sponsor.name} />
 								</div>
 							))}
 						</div>
@@ -47,7 +47,7 @@ export const HomeSponsors = (type) => {
 						<div className="flex flex-wrap gap-4 md:gap-12 items-center justify-center">
 							{GOLD_SPONSORS.map((sponsor) => (
 								<div onClick={() => handleClick(sponsor)} className={`${sponsor.url ? 'cursor-pointer' : ''}`}>
-									<img className="w-24 lg:w-28" src={sponsor.image} alt={sponsor.name} />
+									<img className="w-36" src={sponsor.image} alt={sponsor.name} />
 								</div>
 							))}
 						</div>
